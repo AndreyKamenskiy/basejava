430ff0cc6b8244991d0f1e2d2830fb2c7a50bbe9
Андрей Каменский <rybinsk@mail.ru>
2024-08-15 22:38:34 +0300

    реализованы методы класса ArrayStorage clear, save, get, delete, getAll, size

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,54 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private final Resume[] storage = new Resume[10000];
+    private int size = 0;
 
     void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
     void save(Resume r) {
+        if (r == null || indexOf(r.uuid) != -1) {
+            return;
+        }
+        storage[size++] = r;
     }
 
     Resume get(String uuid) {
-        return null;
+        int index = indexOf(uuid);
+        return index == -1 ? null : storage[index];
     }
 
     void delete(String uuid) {
+        int index = indexOf(uuid);
+        if (index == -1) {
+            return;
+        }
+        storage[index] = storage[--size];
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        return 0;
+        return size;
+    }
+
+    private int indexOf(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid == null && storage[i].uuid == null || storage[i].uuid != null && storage[i].uuid.equals(uuid)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
